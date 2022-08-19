b8006d32218a5ec15de7390795d61f0483881d46
Zvonimir Petrović <zvonimir.petrovic10@gmail.com>
2022-08-19 10:49:59 +0200

    Initial commit

---
+++ src/main/java/com/app/zadatak/ZadatakApplication.java
@@ -0,0 +1,13 @@
+package com.app.zadatak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadatakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadatakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/zadatak/ZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.zadatak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadatakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
