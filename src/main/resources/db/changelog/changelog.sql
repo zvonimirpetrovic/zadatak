303c692ff7fa50c0f3728cc690edf8348ae8bded
Zvonimir Petrović <zvonimir.petrovic10@gmail.com>
2022-08-19 14:06:26 +0200

    Finish setting up liquidbase and clients table

---
+++ src/main/resources/db/changelog/changelog.sql
@@ -0,0 +1,35 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:1
+CREATE TABLE Clients (
+	Id SERIAL PRIMARY KEY, 
+	Name VARCHAR(50) NOT NULL, 
+	Country VARCHAR(50) NOT NULL, 
+	City VARCHAR(50) NOT NULL, 
+	Street_name VARCHAR(50) NOT NULL, 
+	Street_number INT NOT NULL, 
+	Zip INT NOT NULL
+	)
+
+-- changeset liquibase:2
+CREATE TABLE Projects (
+	Id SERIAL PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL, 
+	ClientId INT NOT NULL, 
+	Project_manager VARCHAR(50) NOT NULL, 
+	Contact_email VARCHAR(50), 
+	Contact_number INT, 
+	Billing_address INT NOT NULL
+	)
+	
+-- changeset liquibase:3
+INSERT INTO public.clients
+(name, country, city, street_name, street_number, zip)
+SELECT  
+    'Client ' || n as name, 
+    'Country ' || n as country,
+    'City ' || n as street_name,
+    'Street ' || n as street_name,
+    n as street_number,
+    n*1000 as zip
+ FROM generate_series(1, 100) as n
